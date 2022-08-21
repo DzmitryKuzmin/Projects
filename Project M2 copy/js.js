2c8d067b334d9db9390a2e3b7712074a37e661ef
DzmitryKuzmin <dimonkuzmin1@mail.ru>
2022-08-21 18:49:14 +0300

    first commit

---
+++ Project M2 copy/js.js
@@ -0,0 +1,6 @@
+const addButton = document.querySelector('.add');
+const plus = document.querySelector('.plus');
+let optionList = document.querySelector('.windowList');
+const sortButton = document.querySelector('.sort');
+let deleteButton = document.querySelector('.btnDelete');
+let inputData = document.querySelector('.writeList');
